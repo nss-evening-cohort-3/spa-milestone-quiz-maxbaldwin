b2687966eb2ed7c5052f5c9da710f39759587737
Max Baldwin <maxrbaldwin@gmail.com>
2016-03-26 11:06:20 -0500

    modified sometime earlier

--- CarLot.js
+++ CarLot.js
@@ -1,17 +1,19 @@
-var CarLot = (function () {
-  var inventory = [];
-
+"use strict";
+let CarLot = (function () {
+  let inventory = [];
   return {
-    getInventory: function () {
-
-    },
-    loadInventory: function (callback) {
-      var inventoryLoader = new XMLHttpRequest();
-
+    
+    // getInventory: function () {
+    // },
+    
+    loadInventory: function (callBackToInvoke) {
+      let inventoryLoader = new XMLHttpRequest();
+      inventoryLoader.open("GET", "inventory.json");
+      loader.send();
       inventoryLoader.addEventListener("load", function () {
-
+      inventory = JSON.parse(this.responseText).inventory;
+      callBTI(inventory); 
       });
     }
   };
-
 })();
